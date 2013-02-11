e90bd01f164f3cc8f28fbccdc097f8be43bfa33c
AK <ak@argumenti.ru>
2013-02-11 17:54:47 +0400

    create file array2cache.php

---
+++ array2cache.php
@@ -0,0 +1,32 @@
+<?php
+	
+	function arr2_save($array, $name, $dir = '') {
+		
+		$cfile = ar2c_fname($name, $dir);
+		$code = '<?php $array='.var_export($array, True).'?>';
+		
+		return file_put_contents($cfile, $code);	
+
+	}
+
+	function arr2_array($name, $dir) {
+		
+		$cfile = arr2_fname($name, $dir = '');
+		
+		if (file_exists($cfile)){
+			include($cfile);
+			return $array;
+		}
+
+		return null;
+	} 
+
+
+	function arr2_fname($name, $dir = ''){
+		
+		if ($dir == '' and defined('CACHE_DIR'))
+			$dir = CACHE_DIR;
+
+		return $dir.'arrays/'.sha1($name).'.php';
+	}
+
